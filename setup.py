dfd167308683fa366e20183ba515e52ad9998473
haizhimenhao@outlook.com <haizhimenhao@outlook.com>
2024-08-01 17:55:06 +0800

    Triangle

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-
 import setuptools
- 
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
- 
+
 setuptools.setup(
-    name="triangle",
-    version="0.0.1",
+    name="triangle_calculations_yes",
+    version="0.0.2",
     author="LinQiuyun",
-    author_email="haizhimenhao@outlook.com", 
+    author_email="haizhimenhao@outlook.com",
     description="Triangle calculations",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,4 +19,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'numpy', 
+    ],
 )
+
