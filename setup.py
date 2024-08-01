aa3170761e8b124058a949ac0fca3b8f59b8a333
LinQiuyun <138215900+yesandnoandperhaps@users.noreply.github.com>
2024-08-01 14:32:19 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="triangle",
+    version="0.0.1",
+    author="LinQiuyun",
+    author_email="haizhimenhao@outlook.com", 
+    description="Triangle calculations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yesandnoandperhaps/Triangle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
